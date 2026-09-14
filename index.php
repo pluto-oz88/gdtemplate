996f5867951ff22b66339f7805ce741615e67c4d
Pluto <167601668+pluto-oz88@users.noreply.github.com>
2026-09-14 18:43:12 +1000

    update index.php

--- index.php
+++ index.php
@@ -2,28 +2,28 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="icon" type="image/x-icon" href="img/favicon.ico">
-  <script src="js/day-night.js"></script>
-  <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <script src="js/day-night.js"></script>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
 
-  <?php include 'includes/daynight.php'; ?>
+    <?php include 'includes/daynight.php'; ?>
 
 
-  <div class="wrapper">
-    <h1>Welcome to gdtemplate</h1>
-    <h2>Running on Github desktop too</h2>
-    <h3>We need to add day night button</h3>
-    <p>This is a simple template.</p>
-  </div>
+    <div class="wrapper">
+        <h1>Welcome to gdtemplate</h1>
+        <h2>Running on Github desktop too</h2>
+        <h3>We need to add day night button</h3>
+        <h4>This is a heading 4</h4>
+        <h5>This is a heading 5</h5>
 
-
-
+        <p>This is a simple template.</p>
+    </div>
 </body>
 
 </html>
