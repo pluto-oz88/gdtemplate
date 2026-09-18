9f3742fb25b3fdf22ad8d8c9a2a54dc295a52f90
Graham Dunsby <gdozbiz@outlook.com>
2026-09-18 20:16:14 +1000

    Better commit than commence

--- index.php
+++ index.php
@@ -32,8 +32,14 @@
         <h3>We need to add day night button</h3>
         <h4>This is a heading 4</h4>
         <h5>This is a heading 5</h5>
-
         <p>This is a simple template.</p>
+    </div>
+
+    <div class="vgrid">
+        <div class="topv">Item 1</div>
+        <div class="restv">Item 2</div>
+        <div class="restv">Item 3</div>
+        <div class="restv">Item 4</div>
     </div>
 </body>
 
